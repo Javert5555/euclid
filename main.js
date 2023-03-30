110599e105d233975d73f8ea194275ba27ace7d0
Maxwell Rutherford <javert.hugo@yandex.ru>
2023-03-30 11:49:03 +0300

    fix some errors and add comments

--- main.js
+++ main.js
@@ -1,161 +1,169 @@
-"use strict";
-
-// '5()(1*2 -3)(-5*-5)1(1)';
-// '4*(1*14  - - 4 *-5)*+4/(15/2)';
-// '-1*7 + 15-(-4-4)*(45*6)';
-// '5*5*-5*5*-4/0+2';
-// '-1 +1 -1 +1 -1 +1 -1 + 0 + 0 + 0 - 0';
-// '5-7';
-
-const applyMath = getMathHandler();
-
-// console.log(test0 + ' = ' + applyMath(test0));
-// console.log(test0 + ' = eval не работает.');
-// console.log(test1 + ' == ' + applyMath(test1));
-// console.log(test1 + ' == ' + eval(test1));
-// console.log(test2 + ' == ' + applyMath(test2));
-// console.log(test2 + ' == ' + eval(test2));
-// console.log(test3 + ' == ' + applyMath(test3));
-// console.log(test3 + ' == ' + eval(test3));
-// console.log(test4 + ' == ' + applyMath(test4));
-// console.log(test4 + ' == ' + eval(test4));
-
-console.log(applyMath('5*5*-5*5*-4/0+2'));
-console.log(applyMath('4*(1*14  - - 4 *-5)*+4/(15/2)'));
-
+"use strict"
 
 function getMathHandler() {
-  // получаем объект, где каждая из 4 операций (* + - /) является ключом для функции
-  // модулярных вычислений вместо обычных
-  const math = getMathFn();  
-  let divByZero = false;
-
-  return applyMath;
   
-  /***/
+  /***********************************/
 
   function applyMath(math_str) {
-    divByZero = false;    
-    throwUnmatchedScopes(math_str);
-    
-    math_str = deepRemoveScopes(math_str);    
-    math_str = autoCorrect(math_str);
+    // ставим флаг на false
+    divByZero = false
 
-    let result = parseLinearMath(math_str);
-    return divByZero ? "Ахтунг, деление на ноль" : result;
+    // проверяем количество открывающихся и закрывающихся скобок
+    throwUnmatchedScopes(math_str)
+    
+    // сначала раскрываем все скобки, получая из них результат
+    // то есть: ...+(...)/...-(...+(...)) => ...+.../...-...
+    math_str = deepRemoveScopes(math_str)
+
+    // исправляем всё, что может пойти не так.
+    // в основном, исправляем знаки вида ++, +-, --
+    // по сути убираем всякие невнятности когда в выражении уже нет скобок
+    math_str = autoCorrect(math_str)
+
+    // когда в выражении уже нет скобок просто получаем результат, производя
+    // необходимые модулярные вычисления
+    let result = parseLinearMath(math_str)
+
+    // если где-то начали делить на 0, то выводим ошибку,
+    //  в противном случае возвращаем результат вычислений всего выражения
+    return divByZero ? "Ахтунг, деление на ноль" : result
   }
 
 
   function deepRemoveScopes(str) {
     // очень много replace, делаем корректировку введённой строки 
-    str = autoCorrect(str);
+    str = autoCorrect(str)
     
-    // получаем индекс
-    let index = str.indexOf("(");
-    if( index === -1 ) return parseLinearMath(str);
+    // получаем индекс первой входящей скобки
+    let index = str.indexOf("(")
+
+    // если скобок нет в полученном выражении нет, то возвращаем результат этого выражения,
+    // подсчитанный с помощью функции parseLinearMath
+    if( index === -1 ) return parseLinearMath(str)
     
-    let scope = "(";
-    let open = 1;
+
+    let scope = "("
+
+    // вводим счётчик для подсчёта вхождений открывающихся скобок
+    let open = 1
     
     for( let i = index + 1; i <= 100000; i++ ) {
-      if( i === 100000 ) console.log("Кажется пошел бесконечный цикл");
+      if( i === 100000 ) console.log("Кажется пошел бесконечный цикл")
       
-      scope += str[i];
+      // добавляем следующий символ, идущий после скобки
+      scope += str[i]
       
+      // если следующий символ, после предыдущего, является открывающейся скобкой,
+      // то увеличиваем счётчик для подсчёта вхождений скобок на 1
       if( str[i] === "(" ) {
-        open++;
+        open++
+      // если следующий символ, после предыдущего, является закрывающейся скобкой,
+      // то уменьшаем счётчик для подсчёта вхождений скобок на 1
       } else if( str[i] === ")" ) {
-        open--;
+        open--
       }
       
+      // если по итогу мы получили выражение скобках по типу: (...) или (...(...)) или по типу этого,
+      // то есть число открывающихся скобок равно числу закрывающихся скобок
       if( open === 0 ) {
-        // Привет, рекурсия!
-        // Показалось проще перезапускать функцию после каждой найденной скобки.
-        // При этом учитывая и вложенные скобки scope.slice(1, -1)
-        return deepRemoveScopes( str.replace(scope, deepRemoveScopes( scope.slice(1, -1) ) ) );
+        // то рекурсивно вызаваем функцию, передавая ей ту же строку, только заменяя
+        // вложенную часть со скобками на результат полученный позже из подсчитанного выражения scope,
+        // то есть: deepRemoveScopes( scope.slice(1, -1) )
+        // При этом учитываем и вложенные скобки scope.slice(1, -1), то есть мы просто
+        // обрезаем выражение следующим образом (...) => ... или  (...(...)) => ...(...),
+        // то есть просто выбрасываем внешние скобки
+        return deepRemoveScopes( str.replace(scope, deepRemoveScopes( scope.slice(1, -1) ) ) )
       }
     }
   }
   
-  function parseLinearMath(math_str) { /* уже точно нет скобок */
-    math_str = autoCorrect(math_str);
-    math_str = mul_div(math_str);
-    math_str = plus_minus(math_str);
-
-    return math_str;
+  function parseLinearMath(math_str) {
+    // вы зывается эта функция в случае, если в переданной строке или подстроке нет скобок
 
     /***/
 
     function mul_div(math_str) {
-      let length = (math_str.match(/\/|\*/g) || []).length;
-      if (!length) return math_str;
+      let length = (math_str.match(/\/|\*/g) || []).length
+      if (!length) return math_str
 
       for (let i = 0; i < length; i++) {
         math_str = math_str.replace(
           /(\d+(?:\.\d+)?)(\/|\*)(-?\d+(?:\.\d+)?)/,
           function(_, a, oper, b) {
-            return math(a, oper, b);
+            return math(a, oper, b)
           }
-        );
+        )
 
-        math_str = autoCorrect(math_str);
-        // Строка не из миллиона символов, поэтому после каждой операции
-        // На всякий случай исправляется всё, что может пойти не так.
-        // В основном, "гасятся" знаки вида ++, +-, --
+        // Исправляем всё, что может пойти не так.
+        // В основном, исправляем знаки вида ++, +-, --
+        math_str = autoCorrect(math_str)
       }
 
-      return math_str;
+      return math_str
     }
 
     function plus_minus(math_str) {
-      let length = (math_str.match(/\+|-/g) || []).length;
-      if (!length) return math_str;
+      let length = (math_str.match(/\+|-/g) || []).length
+      if (!length) return math_str
 
       for (let i = 0; i < length; i++) {
         math_str = math_str.replace(
           /((?:^-)?\d+(?:\.\d+)?)(\+|-)(\d+(?:\.\d+)?)/,
           function(_, a, oper, b) {
-            return math(a, oper, b);
+            return math(a, oper, b)
           }
-        );
+        )
 
-        math_str = autoCorrect(math_str);
+        math_str = autoCorrect(math_str)
       }
 
-      return math_str;
+      return math_str
     }
+    math_str = autoCorrect(math_str)
+
+    math_str = mul_div(math_str)
+    
+    math_str = plus_minus(math_str)
+
+    return math_str
   }
 
   function autoCorrect(math_str) {
-    return (math_str               // Замены:
-      .replace(/\s/g, "")          // Удалить все пробелы
-      .replace(/\(\)/g, "")        // Убрать пустые скобки
-      .replace(/--/g, "+")         // Два минуса подряд → Плюс
+    return (math_str               // замены:
+      .replace(/\s/g, "")          // удаляем все пробелы
+      .replace(/\(\)/g, "")        // убрать пустые скобки
+      .replace(/--/g, "+")         // два минуса подряд → плюс
       .replace(/(\+\+|\*\*|\/\/)/g, (_, oper) => oper[0])
-               // Двойные плюсы, умножения и пр → на один
-      .replace(/\+-|-\+/g, "-")    // Плюс после минуса и наоборот → на минус
-      .replace(/\)\(/g, ")*(")     // Две скобки подряд → вставить умножение
-      .replace(/(\d)\(/g, "$1*(")  // Число и сразу скобка → умножение
-      .replace(/\)(\d)/g, ")*$1")  // Скобка и сразу число → умножение
+               // двойные плюсы, умножения и пр → на один
+      .replace(/\+-|-\+/g, "-")    // плюс после минуса и наоборот → на минус
+      .replace(/\)\(/g, ")*(")     // две скобки подряд → вставить умножение
+      .replace(/(\d)\(/g, "$1*(")  // число и сразу скобка → умножение
+      .replace(/\)(\d)/g, ")*$1")  // скобка и сразу число → умножение
       .replace(/(\/|\*)\+/g, "$1") // *+ или /+ → убрать плюс
-    );
+    )
   }
 
   function throwUnmatchedScopes(math_str) {
     // получаем количество открывающихся скобок
-    let scopes_open = (math_str.match(/\(/g) || []).length;
+    let scopes_open = (math_str.match(/\(/g) || []).length
     // получаем количество закрывающихся скобок
-    let scopes_close = (math_str.match(/\)/g) || []).length;
+    let scopes_close = (math_str.match(/\)/g) || []).length
 
     // если количество открывающихся скобок не равно количеству закрывающихся скобок, то пробрасываем ошибку
     if (scopes_open !== scopes_close) {
-      throw new Error("Непарная скобка в " + math_str);
+      throw new Error("Непарная скобка в " + math_str)
     }
   }
 
   function getMathFn() {
     let local_math = {
       "+": (a, b, p=11) => {
+        while (a < 0) {
+          a = Number(a) + p
+        }
+        while (b < 0) {
+          b = Number(b) + p
+        }
         let result = Number(a) + Number(b)
         while (result < 0) {
           result += p
@@ -163,6 +171,12 @@ function getMathHandler() {
         return result % p
       },
       "-": (a, b, p=11) => {
+        while (a < 0) {
+          a = Number(a) + p
+        }
+        while (b < 0) {
+          b = Number(b) + p
+        }
         let result = a - b
         while (result < 0) {
             result += p
@@ -170,6 +184,12 @@ function getMathHandler() {
         return result % p
       },
       "*": (a, b, p=11) => {
+        while (a < 0) {
+          a = Number(a) + p
+        }
+        while (b < 0) {
+          b = Number(b) + p
+        }
         let result = a * b
         while (result < 0) {
           result += p
@@ -177,9 +197,15 @@ function getMathHandler() {
         return result % p
       },
       "/": (a, b, p=11) => {
+        while (a < 0) {
+          a = Number(a) + p
+        }
+        while (b < 0) {
+          b = Number(b) + p
+        }
 
         if( b === "0" ) {
-          divByZero = true;
+          divByZero = true
           // задать вопрос, что делать если делим на 0, ибо тогда верхнее комментим и раскомменчиваем нижнее
           // return 0
         }
@@ -189,6 +215,7 @@ function getMathHandler() {
               return [b, 0, 1]
           }
           else {
+              // пока не дойдём до условия (a == 0), потом функция вернёт [b, 0, 1]
               let [g, x, y] = egcd(b % a, a)
               return [g, y - Math.floor(b / a) * x, x]
           }
@@ -210,10 +237,32 @@ function getMathHandler() {
         return (a * b_reversed % p)
         // ********************************************* \\
       },
-    };
+    }
 
     return function math(a, operation, b) {
-      return local_math[operation](a, b);
+      return local_math[operation](a, b)
     }
   }
+
+  /***********************************/
+
+  // получаем объект, где каждая из 4 операций (* + - /) является ключом для функции
+  // модулярных вычислений вместо обычных
+  const math = getMathFn()  
+  let divByZero = false
+
+  return applyMath
 }
+
+// '5()(1*2 -3)(-5*-5)1(1)'
+// '4*(1*14  - - 4 *-5)*+4/(15/2)'
+// '-1*7 + 15-(-4-4)*(45*6)'
+// '5*5*-5*5*-4/0+2'
+// '-1 +1 -1 +1 -1 +1 -1 + 0 + 0 + 0 - 0'
+// '5-7'
+
+const applyMath = getMathHandler()
+
+console.log(applyMath('5*5*-5*5*-4/0+2'))
+console.log(applyMath('4*(1*14  - - 4 *-5)*+4/(15/2)'))
+console.log(applyMath('3/-7'))
